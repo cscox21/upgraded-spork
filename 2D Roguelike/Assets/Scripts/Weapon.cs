2df08dc2d2e0186d2a7db9ba5abe1de87b780678
Charlie <cscox2191@gmail.com>
2018-04-02 21:20:31 -0500

    Created Weapon
    Created Weapon Script

---
+++ 2D Roguelike/Assets/Scripts/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    
+    public enum Modes
+    { Melee, Straight, Follow, Throw}
+
+    public Sprite sprite;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public float coolDown;
+    public Modes projectileMode;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+}
