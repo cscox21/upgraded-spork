132aa15416b31e45a4217ed4749396c8c735fb30
Charlie <cscox2191@gmail.com>
2018-04-03 22:24:28 -0500

    Moved Arrows
    Arrows fire left just fine
    Arrows dont fire correctly to the right

--- 2D Roguelike/Assets/Scripts/WeaponManager.cs
+++ 2D Roguelike/Assets/Scripts/WeaponManager.cs
@@ -6,34 +6,44 @@ public class WeaponManager : MonoBehaviour {
 
     public GameObject activeWeapon;
     Weapon wpn;
+    bool canShoot = true;
 
-	//Use this for initialization
-	void Start ()
+    //Use this for initialization
+    void Start()
     {
         wpn = activeWeapon.GetComponent<Weapon>();
         GetComponent<SpriteRenderer>().sprite = wpn.sprite;
-	}
-	
-	//Update is called once per frame
-	void Update ()
+    }
+
+    //Update is called once per frame
+    void Update()
     {
-		if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canShoot)
         {
-            Vector3 rotation = transform.parent.localScale.x ==-1 ? Vector3.zero : Vector3.forward * 180;
+            canShoot = false;
+            StartCoroutine("CoolDown");
+
+            Vector3 rotation = transform.parent.localScale.x == -1 ? Vector3.zero : Vector3.forward * 180;
             GameObject projectile = (GameObject)Instantiate(wpn.projectile, transform.position + activeWeapon.transform.GetChild(0).localPosition * transform.parent.localScale.x, Quaternion.Euler(rotation));
 
-            if(wpn.projectileMode == Weapon.Modes.Straight)
-            
+            if (wpn.projectileMode == Weapon.Modes.Straight)
+
                 projectile.GetComponent<Rigidbody2D>().velocity = transform.parent.localScale.x * Vector2.right * wpn.projectileSpeed;
 
-            else if(wpn.projectileMode==Weapon.Modes.Throw)
+            else if (wpn.projectileMode == Weapon.Modes.Throw)
             {
-                    projectile.GetComponent<Rigidbody2D>().isKinematic = false;
+                projectile.GetComponent<Rigidbody2D>().isKinematic = false;
 
-                    projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.parent.localScale.x, 1) * wpn.projectileSpeed;
+                projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.parent.localScale.x, 1) * wpn.projectileSpeed;
             }
 
-            
+
         }
+    }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(wpn.coolDown);
+        canShoot=true;
     }
 }
