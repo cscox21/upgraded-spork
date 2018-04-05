41a1859c01b32dd44f67271dfac998fcd8c586d4
Charlie <cscox2191@gmail.com>
2018-04-04 22:05:14 -0500

    Set up enemy health script
    Created script for enemy health
    Can now take damage
    Projectiles still dont damage enemy

--- 2D Roguelike/Assets/Scripts/Enemy_Health.cs
+++ 2D Roguelike/Assets/Scripts/Enemy_Health.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class Enemy_Health : MonoBehaviour {
 
-	void Update ()
+    public int startingHealth = 100;
+    public int currentHealth;
+    public int scoreValue;
+
+    bool isDead;
+    BoxCollider2D boxCollider2D;
+
+    private void Awake()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        currentHealth = startingHealth;
+    }
+
+    void Update ()
     {
 	    if(gameObject.transform.position.y <-10)
         {
             Destroy(gameObject);
         }
 	}
+
+    public void TakeDamage(int amount, Vector2 hitPoint)
+    {
+        if(isDead)
+        
+            return;
+        currentHealth -= amount;
+        if(currentHealth<=0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        boxCollider2D.isTrigger = true;
+        Destroy(gameObject, 2f);
+    }
+
 }
