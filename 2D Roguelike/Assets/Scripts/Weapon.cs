8e38a859dc96b3540ebbfa7459023474edb15bec
Charlie <cscox2191@gmail.com>
2018-04-08 17:25:48 -0500

    Deleted bad code
    Deleted unworking code for projectiles

--- 2D Roguelike/Assets/Scripts/Weapon.cs
+++ 2D Roguelike/Assets/Scripts/Weapon.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
     
-    public enum Modes
+    public enum Modes 
     { Melee, Straight, Follow, Throw}
 
-    public Sprite sprite;
-    public GameObject projectile;
-    public float projectileSpeed;
-    public float coolDown;
+    public Sprite sprite; //reference to sprite image
+    public GameObject projectile; //reference to projectile
+    public float projectileSpeed; //velocity of projectiles
+    public float coolDown; //time between attacks 
     public Modes projectileMode;
+
 
     public int damagePerShot = 50;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
 
 	}
+
 
 }
