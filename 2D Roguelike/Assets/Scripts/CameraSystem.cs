83e6018fca642357e16e8b63a34b760f7ed7ba92
Charlie <cscox2191@gmail.com>
2018-04-13 21:45:02 -0500

    Created 2nd Scene
    Created character select scene
    created 2 scripts for character select

--- 2D Roguelike/Assets/Scripts/CameraSystem.cs
+++ 2D Roguelike/Assets/Scripts/CameraSystem.cs
@@ -13,16 +13,24 @@ public class CameraSystem : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        
 	}
 	
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        //clamps camera on x position
-        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
-        //clamps camera on y position
-        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
-        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            //clamps camera on x position
+            float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+            //clamps camera on y position
+            float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+            gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+        }
+
     }
 }
