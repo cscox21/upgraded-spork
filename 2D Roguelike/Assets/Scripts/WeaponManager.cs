ae3acefd881b84534341fa139594b27c424fa953
Charlie <cscox2191@gmail.com>
2018-04-22 14:49:33 -0500

    Updating Fireball code
    Trying to fix fireball code
    added moveX variable
    moveX variable doesnt find player's current position

--- 2D Roguelike/Assets/Scripts/WeaponManager.cs
+++ 2D Roguelike/Assets/Scripts/WeaponManager.cs
@@ -7,13 +7,15 @@ public class WeaponManager : MonoBehaviour {
     public GameObject activeWeapon;
     Weapon wpn;
     bool canShoot = true;
+    public float moveX;
 
     //Use this for initialization
     void Start()
     {
-
+        moveX = transform.position.x;
         wpn = activeWeapon.GetComponent<Weapon>();
         GetComponent<SpriteRenderer>().sprite = wpn.sprite;
+        
     }
 
     //Update is called once per frame
@@ -24,17 +26,37 @@ public class WeaponManager : MonoBehaviour {
             canShoot = false;
             StartCoroutine("CoolDown");
 
-            Vector3 rotation = transform.parent.localScale.x == -1 ? Vector3.zero : Vector3.forward * 180;
-            GameObject projectile = (GameObject)Instantiate(wpn.projectile, transform.position + activeWeapon.transform.GetChild(0).localPosition * transform.parent.localScale.x, Quaternion.Euler(rotation));
 
-            if (wpn.projectileMode == Weapon.Modes.Straight)
-
-            projectile.GetComponent<Rigidbody2D>().velocity = transform.parent.localScale.x * Vector2.right * wpn.projectileSpeed;
-
-            else if (wpn.projectileMode == Weapon.Modes.Throw)
+            if (moveX > transform.localPosition.x)
             {
+                Vector3 rotation = transform.parent.localScale.x == -1 ? Vector3.zero : Vector3.forward * 180;
+                GameObject projectile = (GameObject)Instantiate(wpn.projectile, transform.position + activeWeapon.transform.GetChild(0).localPosition * transform.parent.localScale.x, Quaternion.Euler(rotation));
+                if (wpn.projectileMode == Weapon.Modes.Straight)
 
-                projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.parent.localScale.x, -1) * wpn.projectileSpeed;
+                    projectile.GetComponent<Rigidbody2D>().velocity = transform.parent.localScale.x * Vector2.right * wpn.projectileSpeed;
+
+                else if (wpn.projectileMode == Weapon.Modes.Throw)
+                {
+
+                    projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.parent.localScale.x, -1) * wpn.projectileSpeed;
+                }
+            }
+
+            else if (moveX < transform.localPosition.x)
+            {
+                Vector3 rotation = transform.parent.localScale.x == 1 ? Vector3.zero : Vector3.forward * 180;
+                GameObject projectile = (GameObject)Instantiate(wpn.projectile, transform.position + activeWeapon.transform.GetChild(0).localPosition * transform.parent.localScale.x, Quaternion.Euler(rotation));
+
+
+                if (wpn.projectileMode == Weapon.Modes.Straight)
+
+                    projectile.GetComponent<Rigidbody2D>().velocity = transform.parent.localScale.x * Vector2.left * wpn.projectileSpeed;
+
+                else if (wpn.projectileMode == Weapon.Modes.Throw)
+                {
+
+                    projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.parent.localScale.x, -1) * wpn.projectileSpeed;
+                }
             }
 
 
