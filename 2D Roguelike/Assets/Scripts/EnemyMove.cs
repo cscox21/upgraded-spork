be1eb44c2b4dd657c161a6627293d4ed91c835b9
Charlie <cscox2191@gmail.com>
2018-04-22 22:03:40 -0500

    Added Boss
    Added Boss to scene

--- 2D Roguelike/Assets/Scripts/EnemyMove.cs
+++ 2D Roguelike/Assets/Scripts/EnemyMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyMove : MonoBehaviour {
 
@@ -21,6 +22,7 @@ public class EnemyMove : MonoBehaviour {
             if(hit.collider.tag == "Player")
             {
                 Destroy(hit.collider.gameObject);
+                SceneManager.LoadScene("Level_01");
             }
         }
 	}
