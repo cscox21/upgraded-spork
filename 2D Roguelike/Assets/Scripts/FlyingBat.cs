c55872a3071ecd63694fe3c9605070584c2c4b59
Charlie <cscox2191@gmail.com>
2018-05-06 21:13:17 -0500

    Created flying enemy
    created flying bat enemy.
    Still needs patrol points
    Needs to kill player

---
+++ 2D Roguelike/Assets/Scripts/FlyingBat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBat : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float amplitude;
+
+    private Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tempPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
+	}
+}
