edbe32d9897b8ae2e7f4231488afe42e7af7c3fc
Charlie <cscox2191@gmail.com>
2018-05-07 22:04:32 -0500

    Tried to fix bat script
    Tried to redo bat script to have flying patrol, or swoop like effect. Got close, no success yet.

--- 2D Roguelike/Assets/Scripts/FlyingBat.cs
+++ 2D Roguelike/Assets/Scripts/FlyingBat.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class FlyingBat : MonoBehaviour {
 
-    public float horizontalSpeed;
-    public float verticalSpeed;
-    public float amplitude;
+    
+    public float horizontalSpeed;   
+    public float verticalSpeed;   
+    public float amplitude;    
 
-    private Vector3 tempPosition;
+    public Transform[] patrolpoints;
+    int currentPoint;
 
-	// Use this for initialization
-	void Start ()
+    private Vector3 tempPosition; 
+
+    // Use this for initialization
+    void Start ()
     {
         tempPosition = transform.position;
+
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         tempPosition.x += horizontalSpeed;
         tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
         transform.position = tempPosition;
-	}
+
+        if (tempPosition.x < patrolpoints[currentPoint].position.x)
+            transform.localScale = Vector3.one;
+            //horizontalSpeed = -horizontalSpeed;
+            //tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+            //transform.position = tempPosition;
+
+        
+
+        else if (tempPosition.x > patrolpoints[currentPoint].position.x)
+        transform.localScale = new Vector3(-1, 1, 1);
+        //tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        //transform.position = tempPosition;
+
+
+    }
+
+   
 }
