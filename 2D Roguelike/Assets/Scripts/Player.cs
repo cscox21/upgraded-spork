fe16cc6382ddd6e7d28dd90dbf2142ae84d45541
Charlie <cscox2191@gmail.com>
2018-05-29 19:35:32 -0500

    Created Health For Player
    Created health for player and redid how player can die.

---
+++ 2D Roguelike/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [System.Serializable]
+    public class PlayerStats
+    {
+        public int Health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -30;
+
+    void Update()
+    {
+        if(transform.position.y <= -30)
+        {
+            DamagePlayer(999999);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        playerStats.Health -= damage;
+        if(playerStats.Health <= 0f)
+        {
+            GameController.KillPlayer(this);
+        }
+    }
+
+}
