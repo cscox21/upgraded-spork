2f160ce4f175b567ccde2acc7aeef95e6a3a8618
Charlie <cscox2191@gmail.com>
2018-07-03 17:41:29 -0500

    Altering Rays
    Trying to get rays to work properly. Currently the main ray is hit objects that arent in its view

--- 2D Roguelike/Assets/Scripts/AimedEnemyShots.cs
+++ 2D Roguelike/Assets/Scripts/AimedEnemyShots.cs
@@ -14,53 +14,39 @@ public class AimedEnemyShots : MonoBehaviour {
 
     //Trying multiple rays
     public float headHeight = 1f;
-    public float fovAngle;
     public float sight = 3f;
+    public float topSight = 5f;
 
 	// Use this for initialization
 	void Start ()
     {
         fireRate = 1f;
         nextFire = Time.time;
-       
-
-
-        //Vector3 rayPosition = new Vector3(transform.position.x, headHeight, transform.position.z);
-        //Vector3 leftRayRotation = Quaternion.AngleAxis(-fovAngle, transform.up) * transform.forward;
-        //Vector3 rightRayRotation = Quaternion.AngleAxis(fovAngle, transform.up) * transform.forward;
-
-        //Constructing rays
-
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         Debug.DrawRay(transform.position + Vector3.left * headHeight, transform.right * -sight, Color.green);
-        Debug.DrawRay(transform.position + Vector3.left * headHeight, (transform.right + transform.up).normalized * -sight, Color.blue);
-        Debug.DrawRay(transform.position + Vector3.left * headHeight, (transform.right - transform.up).normalized * -sight, Color.blue);
+        //Debug.DrawRay(transform.position + Vector3.left * headHeight, (transform.right + transform.up).normalized * -sight, Color.blue);
+        Debug.DrawRay(transform.position + Vector3.left * headHeight, (transform.right - transform.up).normalized * -topSight, Color.blue);
         CheckIFTimeToFire();
 	}
 
     void CheckIFTimeToFire()
     {
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.localScale.x * Vector2.right, sight);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.left * headHeight, -transform.right * sight);
+        RaycastHit2D topHit = Physics2D.Raycast(transform.position + Vector3.left * headHeight, (transform.right - transform.up).normalized * -topSight);
 
-
-
-
-        //Vector3 rayPosition = new Vector3(transform.position.x, headHeight, transform.position.z);
-        //Vector3 leftRayRotation = Quaternion.AngleAxis(-fovAngle, transform.up) * transform.forward;
-        //Vector3 rightRayRotation = Quaternion.AngleAxis(fovAngle, transform.up) * transform.forward;
-
-        //Ray rayCenter = new Ray(rayPosition, transform.forward);
-        //Ray rayLeft = new Ray(rayPosition, leftRayRotation);
-        //Ray rayRight = new Ray(rayPosition, rightRayRotation);
-
-        if (Time.time > nextFire) //&& rayCenter.collider.tag == "Player")
+        if (hit.collider != null && hit.collider.tag == "Player") 
         {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            nextFire = Time.time + fireRate;
+            Debug.Log("Attack the Player");
+            if (Time.time > nextFire)
+            { 
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                nextFire = Time.time + fireRate;
+            }
         }
     }
 }
