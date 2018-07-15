0739136e76bebd9227d4e62559dce26d27ce5d95
Charlie <cscox2191@gmail.com>
2018-07-15 17:46:07 -0500

    Screen Flashes
    Screen flashes when player is hit

--- 2D Roguelike/Assets/Scripts/DamagedFlash.cs
+++ 2D Roguelike/Assets/Scripts/DamagedFlash.cs
@@ -4,10 +4,4 @@ using UnityEngine;
 
 public class DamagedFlash : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
-    {
-        gameObject.SetActive(false);
-        other.gameObject.GetComponent<HurtPlayer>().HurtPlayer(damageToGive);
-    }
 }
--- 2D Roguelike/Assets/Scripts/Player.cs
+++ 2D Roguelike/Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public int playerMaxHealth; //starting health of enemy
     public int playerCurrentHealth; //current health of enemy
+    bool damaged;
+    public Image damageImage;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.3f);
+    public float flashSpeed = 5f;
 
     private void Start()
     {
@@ -16,6 +20,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (damaged)
+        {
+            //...set the color of the damageImage to the flash color.
+            damageImage.color = flashColour;
+        }
+        //otherwise...
+        else
+        {
+            //...transition the color back to clear
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        //reset the damaged flag.
+        damaged = false;
+        
+
         if (playerCurrentHealth <= 0)
         {
             Destroy(gameObject);
@@ -30,11 +49,13 @@ public class Player : MonoBehaviour
     public void HurtPlayer(int damageToGive)
     {
         Debug.Log("hurt the player with " + damageToGive + " damage in hitpoints");
-
+        damaged = true;
         playerCurrentHealth -= damageToGive;
     }
     public void MaxHealth()
     {
         playerCurrentHealth = playerMaxHealth;
     }
+
+    
 }
