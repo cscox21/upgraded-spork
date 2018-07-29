79793caff57e44d82919fb5500753f66e1343599
Charlie <cscox2191@gmail.com>
2018-07-29 15:00:02 -0500

    Created basic spawn trigger
    Created area where enemies will spawn once reached. Need to alter a bit.

---
+++ 2D Roguelike/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject enemy;
+    public Transform enemyPos;
+    float spawnRate = 3.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        InvokeRepeating("EnemySpawner", 0.5f, spawnRate);
+        Destroy(gameObject, 11);
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    void EnemySpawner()
+    {
+        Instantiate(enemy, enemyPos.position, enemyPos.rotation);
+    }
+}
