e304fc13a1f7a05762e2df5f9cbff445ce25026d
Charlie <cscox2191@gmail.com>
2018-08-05 17:05:30 -0500

    Changing scene 1
    Trying to work backwards to figure out shooting issue with Scene 01.

--- 2D Roguelike/Assets/Scripts/Player/ArmRotation.cs
+++ 2D Roguelike/Assets/Scripts/Player/ArmRotation.cs
@@ -8,6 +8,7 @@ public class ArmRotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        Debug.Log("We are rotating the arm");
         //subtracting the position of the player from the mouse position.
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;   
         difference.Normalize();  //normailizing the vector. Meaning that all the sum of the vector will be equal to 1.
@@ -16,5 +17,6 @@ public class ArmRotation : MonoBehaviour {
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+        
     }
 }
