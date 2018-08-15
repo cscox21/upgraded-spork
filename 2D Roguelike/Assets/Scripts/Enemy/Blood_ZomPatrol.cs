61e573299d250c52aa79f810fd1caebe5942dfb6
Charlie <cscox2191@gmail.com>
2018-08-14 20:43:37 -0500

    Changed Bloody Zombie
    Change the bloody zombie. Attack is different, patrols now, has animations.

---
+++ 2D Roguelike/Assets/Scripts/Enemy/Blood_ZomPatrol.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blood_ZomPatrol : MonoBehaviour
+{
+
+    public Transform[] patrolpoints;
+    int currentPoint;
+    public float speed = 0.5f;
+    public float timeStill = 1.2f;
+    public float sight = 5f;
+    Animator anim;
+
+    public GameObject projectile;
+    public Transform firePos;
+
+    float nextBasicAttack;
+    public float basicAttackRate = .25f;
+
+    public float headHeight;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine("Patrol");
+        //basicAttackRate = .3f;
+        nextBasicAttack = Time.time;
+        anim.SetBool("Walking", true);
+
+    }
+
+    IEnumerator Patrol()
+    {
+        while (true)
+        {
+            if (transform.position.x == patrolpoints[currentPoint].position.x)
+            {
+                currentPoint++;
+                anim.SetBool("Walking", false);
+                yield return new WaitForSeconds(timeStill);
+                anim.SetBool("Walking", true);
+            }
+
+            if (currentPoint >= patrolpoints.Length)
+            {
+                currentPoint = 0;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(patrolpoints[currentPoint].position.x, transform.position.y), speed);
+
+            Debug.DrawRay(transform.position + Vector3.up * headHeight, transform.right * -sight, Color.green);
+            Debug.DrawRay(transform.position + Vector3.up * headHeight, transform.right * sight, Color.blue);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * headHeight, transform.right, sight);
+            RaycastHit2D rightHit = Physics2D.Raycast(transform.position + Vector3.up * headHeight, transform.right, -sight);
+
+            if (transform.localScale.x ==-1)
+            {
+                if (hit.collider != null && hit.collider.tag == "Player")
+                    
+                {
+                    Shoot();
+                }
+            }
+            if (transform.localScale.x ==1)
+            {
+                if (rightHit.collider != null && rightHit.collider.tag == "Player")
+                    //if (rightHit.collider != null && rightHit.collider.tag == "Player")
+                {
+                    Shoot();
+                }
+            }
+            
+
+            if (transform.position.x < patrolpoints[currentPoint].position.x)
+                transform.localScale = Vector3.one;
+            else if (transform.position.x > patrolpoints[currentPoint].position.x)
+                //transform.localScale = Vector3.one;
+                transform.localScale = new Vector3(-1, 1, 1);
+            yield return null;
+        }
+    }
+
+    void Shoot()
+    {
+        if (Time.time > nextBasicAttack)
+        {
+            Instantiate(projectile, firePos.position, Quaternion.identity);
+            nextBasicAttack = Time.time + basicAttackRate;
+        }
+    }
+    /*
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.localScale.x * Vector3.right * -sight);
+    }
+    */
+}
---
+++ 2D Roguelike/Assets/Scripts/Objects/BloodZom_Attack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodZom_Attack : MonoBehaviour {
+
+    Player target;
+    Rigidbody2D rb;
+    
+
+    void Start()
+    {
+        float xPower = Random.Range(150f, 300f);
+        float yPower = Random.Range(125f, 225f);
+
+        rb = GetComponent<Rigidbody2D>();
+        target = FindObjectOfType<Player>();
+
+        
+        if (target.transform.position.x < transform.position.x)
+        {
+            rb.AddForce(transform.up * yPower);
+            rb.AddForce(transform.right * -xPower);
+        }
+        else
+        {
+            rb.AddForce(transform.up * yPower);
+            rb.AddForce(transform.right * xPower);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "ground")
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("SpecialFire Hit");
+            Destroy(gameObject);
+        }
+    }
+}
