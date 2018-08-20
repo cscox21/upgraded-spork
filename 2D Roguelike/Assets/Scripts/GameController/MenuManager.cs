a7bb6fd4b37f3e036a082ae216f465b90b4bf96c
Charlie <cscox2191@gmail.com>
2018-08-19 19:58:06 -0500

    Trying to give buttons functions
    Working on making start and end game buttons work properly

---
+++ 2D Roguelike/Assets/Scripts/GameController/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene_02");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("WE ARE QUITTING THE GAME");
+        Application.Quit();
+    }
+
+}
+
