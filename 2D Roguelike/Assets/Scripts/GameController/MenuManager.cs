6f85e08e2e29e3aa173ad92d38a45018d88bf926
Charlie <cscox2191@gmail.com>
2018-08-20 21:11:27 -0500

    Level Select Scene
    Have level 1 and 2 buttons working

--- 2D Roguelike/Assets/Scripts/GameController/MenuManager.cs
+++ 2D Roguelike/Assets/Scripts/GameController/MenuManager.cs
@@ -11,11 +11,35 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene(scene, LoadSceneMode.Single);
     }
 
+
     public void QuitGame()
     {
         Debug.Log("WE ARE QUITTING THE GAME");
         Application.Quit();
     }
 
+    public void LoadLevelOne()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    public void LoadLevelTwo()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex + 2;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    public void LoadLevelThree()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex + 3;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    public void LoadLevelFour()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex + 4;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
 }
 
