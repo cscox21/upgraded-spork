0338a718f38123d258c8b7bdd649ef8f203a14be
Charlie <cscox2191@gmail.com>
2018-09-03 16:38:18 -0500

    Working with arm rotation
    Working to get arm rotation with perspective camera.

--- 2D Roguelike/Assets/Scripts/Player/ArmRotation.cs
+++ 2D Roguelike/Assets/Scripts/Player/ArmRotation.cs
@@ -11,12 +11,16 @@ public class ArmRotation : MonoBehaviour {
         //subtracting the position of the player from the mouse position.
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
+        //var ray = Camera.main.ScreenPointToRay(Input.GetTouch().position);
+
         difference.Normalize();  //normailizing the vector. Meaning that all the sum of the vector will be equal to 1.
 
         //find the angle in degrees
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+
         
+
     }
 }
